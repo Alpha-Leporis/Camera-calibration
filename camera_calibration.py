d5be1555294cc1b75186baca27f1d01cb690858b
ABHIJEET RATHORE <34433950+itsmeaby@users.noreply.github.com>
2022-02-02 16:49:45 +0530

    Update camera_calibration.py

--- camera_calibration.py
+++ camera_calibration.py
@@ -10,7 +10,7 @@ import cv2
 import numpy as np
 import glob
 
-# Dimensions of checkerboard
+# Defining the dimensions of checkerboard
 CHECKERBOARD = (6,8)
 
 # Defining the algorithm termination criteria
@@ -18,15 +18,15 @@ CHECKERBOARD = (6,8)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # Arrays to store object points and image points from all the images.
-objpoints = []      # 3d point in real world space
-imgpoints = []      # 2d points in image plane
+objpoints = []      # 3d point in real world space (vector)
+imgpoints = []      # 2d points in image plane (vector)
 
 # Defining the world coordinates for 3D points (Preparing object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0) )
 objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
 objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
-
+# Extracting path of individual image stored in a given directory
 images = glob.glob('./img/*.jpg')
 for fname in images:
     img = cv2.imread(fname)
@@ -35,10 +35,15 @@ for fname in images:
     # Finding the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
     
-    # If found, add object points, image points (after refining them)
+    """
+	    If desired number of corner are detected,
+	    we refine the pixel coordinates and display
+	    them on the images of checker board
+	"""
     if ret == True:
         objpoints.append(objp)
         
+        # Refining pixel coordinates for given 2d points.
         corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
         
         imgpoints.append(corners2)
@@ -53,7 +58,12 @@ cv2.destroyAllWindows()
 
 h,w = img.shape[:2]
 
-# Calibrating Camera
+"""
+	Performing camera calibration by
+	passing the value of known 3D points (objpoints)
+	and corresponding pixel coordinates of the
+	detected corners (imgpoints)
+"""
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 print("Camera matrix : \n")
